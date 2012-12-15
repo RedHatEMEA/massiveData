aa583b0e83fd3470bbb11559521f277ad3a35dc0
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2012-12-15 18:34:59 +0000

    Added utils for Item name handling, JUnit test (not completed)

---
+++ elucidusCore/src/org/elucidus/currency/utils/tests/ItemNameToolsTest.java
@@ -0,0 +1,61 @@
+package org.elucidus.currency.utils.tests;
+
+import static org.junit.Assert.*;
+
+import org.elucidus.currency.utils.ItemNameTools;
+import org.junit.Test;
+
+public class ItemNameToolsTest
+{
+  private static final String EXPECTED_UUID = "1111-2222-3333";  
+  private static final String EXPECTED_UUID_UTC = "1111-2222-3333_1234567890";
+
+  private static final String VALID_NAME = "myDataCache.1111-2222-3333.myField";
+  private static final String VALID_NAME_UTC = "myDataCache.1111-2222-3333_1234567890.myField";
+  private static final String INVALID_NAME_TOO_FEW_TOKENS = "myDataCache.1111-2222-3333";
+  private static final String VALID_UUID = "1111-2222-3333";
+  private static final String VALID_UUID_UTC = "1111-2222-3333_1234567890";
+  private static final String VALID_NAME_MULTIPLE_FIELDS = "myDataCache.1111-2222-3333.fieldLevel1.fieldLevel2";
+
+  @Test
+  public void testGetUUID()
+  {
+    String testUUID = null;
+    
+    try
+    {
+      testUUID = ItemNameTools.getUUID(VALID_NAME);
+      
+      assertTrue( testUUID.equals( EXPECTED_UUID ) );      
+    }
+    catch( Exception exc )
+    {
+      fail("Exception occured : " + exc.toString());
+    }
+  }
+
+  @Test
+  public void testExtractUUIDEmbeddedUTC()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGenerateUUID()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetCacheName()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetFieldComponents()
+  {
+    fail("Not yet implemented");
+  }
+
+}
