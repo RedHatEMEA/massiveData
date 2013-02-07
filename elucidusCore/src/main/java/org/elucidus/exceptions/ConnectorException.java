78c2413921a7723383c9c7017974c372259e3d40
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2013-02-07 14:51:48 +0000

    New package - org.elucidus.connectors.xxxx - for specific code for external connectors (such as LUCENE) that can be used by any of the other packages (i.e. utils for both Persisters and Surfacers). Added the first connector util class for LUCENE3 containing *all* the helper methods for adding, removing, reporting and maintaining Indices of Documents generated from Items.

---
+++ elucidusCore/src/main/java/org/elucidus/exceptions/ConnectorException.java
@@ -0,0 +1,35 @@
+package org.elucidus.exceptions;
+
+/**
+ * This class provides an exception for when a connector actionn fails.
+ * @author Ian Lawson (<a href="mailto:ian.lawson@redhat.com">ian.lawson@redhat.com</a>).
+ */
+public class ConnectorException extends Exception
+{
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Standard no parameter constructor - calls the super() method from {@link java.lang.Exception java.lang.Exception}.
+   */
+  public ConnectorException()
+  {
+    super();
+  }
+
+  /**
+   * Standard message parameter constructor - calls the super() method from {@link java.lang.Exception java.lang.Exception}.
+   * @param message message to add to the exception object
+   */
+  public ConnectorException( String message )
+  {
+    super( message );
+  } 
+  
+  public ConnectorException(Exception cause) 
+  {
+	  super(cause); 
+  }
+}
