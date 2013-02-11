3b3fc7939b78bb550360cf18297b6101768c26cc
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2013-02-11 08:39:08 +0000

    Added two constants to standard aspects for *mandatory* fields. These are SOURCE, which contains a string based definition of the originating source (i.e. uri, url, fully qualified filename etc) and GENERATOR which should contain the class that was used to create the item.

--- elucidusCore/src/main/java/org/elucidus/generation/StandardAspects.java
+++ elucidusCore/src/main/java/org/elucidus/generation/StandardAspects.java
@@ -1,6 +1,10 @@
 package org.elucidus.generation;
 
-public class StandardAspects {
+public class StandardAspects 
+{
+  public static final String SOURCE = "elucidus_source";
+  public static final String GENERATOR = "elucidus_generator";
+  
 	public static final String FILE_FILENAME = "file.filename";
 	public static final String FILE_SIZE_BYTES = "file.sizeBytes";
 	public static final String FILE_MODIFIED_UTC = "file.modifiedUtc";  
