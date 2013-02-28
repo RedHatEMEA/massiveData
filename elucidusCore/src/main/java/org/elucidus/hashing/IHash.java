a4c24933e2badc819d4506dd44750a4fd27b5c40
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2013-02-28 18:14:42 +0000

    Remove previous source for 'elucidus'

--- elucidusCore/src/main/java/org/elucidus/hashing/IHash.java
+++
@@ -1,9 +0,0 @@
-package org.elucidus.hashing;
-
-import org.elucidus.exceptions.HashingException;
-
-public interface IHash
-{
-  public String hash( String input ) throws HashingException;
-  public byte[] hash( byte[] input ) throws HashingException;
-}
